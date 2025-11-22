c40b0366c532e026be54998fa7bb58e9844a9e1b
oyurekten <ozgury@ebi.ac.uk>
2025-11-22 12:11:53 +0000

    package update

--- scripts/update.py
+++ scripts/update.py
@@ -29,33 +29,7 @@ if __name__ == "__main__":
     update_controls()
     rules = get_rules()
     create_summary_files(rules)
-    subprocess.run(["./build_bundle.sh"], check=True)
-
-    # assay_controlled_terms = create_recommended_assay_control_lists()
-
-    # templates_path = (
-    #     "validation/metabolights/validation/v2/controlLists/investigationFile"
-    # )
-    # file_name = "investigation-file"
-    # title = "Investigation File"
-    # create_recommended_control_lists(
-    #     templates_path=templates_path, file_name=file_name, title=title
-    # )
-    # templates_path = "validation/metabolights/validation/v2/controlLists/sampleColumns"
-    # file_name = "sample-file"
-    # title = "Sample File"
-    # sample_controlled_terms = create_recommended_control_lists(
-    #     templates_path=templates_path, file_name=file_name, title=title
-    # )
-
-    # templates_path = "validation/metabolights/validation/v2/controlLists/unitColumns"
-    # file_name = "unit-columns"
-    # title = "Unit Columns"
-    # create_recommended_control_lists(
-    #     templates_path=templates_path, file_name=file_name, title=title
-    # )
-
-    # create_file_structure_documentation(
-    #     assay_controlled_terms=assay_controlled_terms,
-    #     sample_controlled_terms=sample_controlled_terms,
-    # )
+    try:
+        result = subprocess.run(["/bin/bash", "./build_bundle.sh"], check=True)
+    except Exception as ex:
+        print(ex)
