423ec5b24a0295a49ae17f31fb12f8a0bb81e92d
oyurekten <ozgury@ebi.ac.uk>
2025-12-05 11:00:55 +0000

    add missing module

---
+++ scripts/validation_models.py
@@ -0,0 +1,19 @@
+import enum
+
+from pydantic import BaseModel
+
+
+class PolicyMessageType(str, enum.Enum):
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+    INFO = "INFO"
+    SUCCESS = "SUCCESS"
+
+
+class Violation(BaseModel):
+    rule_id: str = ""
+    title: str = ""
+    description: str = ""
+    type: PolicyMessageType = PolicyMessageType.INFO
+    section: str = ""
+    priority: str = ""
